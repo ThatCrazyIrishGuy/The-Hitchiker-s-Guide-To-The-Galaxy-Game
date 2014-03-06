c769ae56a48aac4b313bc27667e97b6da6a51b1f
Darren Britton <darrenbritton@hotmail.com>
2014-03-06 15:43:12 +0000

    fixed gitignore file

--- Assets/Scripts/Sidescroller/GameManager.cs
+++ Assets/Scripts/Sidescroller/GameManager.cs
@@ -4,15 +4,15 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
 	
 	public GameObject player;
-	private GameCamera cam;
+	//private GameCamera cam;
 	
 	void Start () {
-		cam = GetComponent<GameCamera>();
+		//cam = GetComponent<GameCamera>();
 		SpawnPlayer();
 	}
 	
 	// Spawn player
 	private void SpawnPlayer() {
-		cam.SetTarget((Instantiate(player,Vector3.zero,Quaternion.identity) as GameObject).transform);
+		//cam.SetTarget((Instantiate(player,Vector3.zero,Quaternion.identity) as GameObject).transform);
 	}
 }
